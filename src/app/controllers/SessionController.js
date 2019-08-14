37198657cf7b3d58569339ba3abc5b37ab1cc21c
Nilo César <e_maildonilo@yahoo.com.br>
2019-08-14 01:00:24 -0300

    save

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,6 +3,7 @@ import * as Yup from 'yup';
 
 import authConfig from '../../config/auth';
 import User from '../models/User';
+import File from '../models/File';
 
 class SessionController {
   async store(req, res) {
@@ -21,6 +22,13 @@ class SessionController {
 
     const user = await User.findOne({
       where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
     });
 
     if (!user) {
@@ -31,10 +39,10 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not mach' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
-      user: { id, name, email },
+      user: { id, name, email, avatar, provider },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
       }),
